5702ed44884e4cd073e4fed0fc5a79a2538428a4
Vasiliy Yudin <vvyudin@avito.ru>
2021-01-21 16:55:10 +0300

    CTHL-1515 remove megacheck and change repo name

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -19,8 +19,4 @@ echo "golint:"
 OUT=$(golint $PKG/... 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
-echo "megacheck:"
-OUT=$(megacheck $PKG/... 2>&1 | grep --invert-match -E "(example/)")
-if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
-
 if [ -n "$PROBLEM" ]; then exit 1; fi
