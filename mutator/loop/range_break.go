072d4368dad10a3420107d3a49284f494908b192
Vasiliy Yudin <vvyudin@avito.ru>
2021-02-18 16:12:12 +0300

    CTHL-1550 improved version for range-based-for

--- mutator/loop/range_break.go
+++ mutator/loop/range_break.go
@@ -12,70 +12,33 @@ func init() {
 	mutator.Register("loop/range_break", MutatorLoopRangeBreak)
 }
 
-func checkIsRangeStatement(node ast.Stmt) bool {
-	switch node.(type) {
-	case *ast.RangeStmt:
-		return true
-	}
-
-	return false
-}
-
 // MutatorLoopRangeBreak implements a mutator to add a break to range-loop body.
 func MutatorLoopRangeBreak(_ *types.Package, _ *types.Info, node ast.Node) []mutator.Mutation {
-	var list []ast.Stmt
-
-	switch n := node.(type) {
-	case *ast.BlockStmt:
-		list = n.List
-	case *ast.CaseClause:
-		list = n.Body
-	default:
+	n, ok := node.(*ast.RangeStmt)
+	if !ok {
 		return nil
 	}
 
-	var mutations []mutator.Mutation
+	newBody := &ast.BlockStmt{
+		List: []ast.Stmt{},
+	}
+	oldBody := n.Body
 
-	for i, ni := range list {
-		if checkIsRangeStatement(ni) {
-			listIndex := i
-			oldNode := list[listIndex]
-			rangeNode, ok := oldNode.(*ast.RangeStmt)
-			if !ok {
-				return nil
-			}
+	newBreakStmt := &ast.BranchStmt{Tok: token.BREAK}
+	newBody.List = append(newBody.List, newBreakStmt)
 
-			newBody := &ast.BlockStmt{
-				List: []ast.Stmt{},
-			}
-
-			newBreakStmt := &ast.BranchStmt{Tok: token.BREAK}
-			newBody.List = append(newBody.List, newBreakStmt)
-
-			for _, nn := range rangeNode.Body.List {
-				newBody.List = append(newBody.List, nn)
-			}
-
-			newNode := &ast.RangeStmt{
-				For:    rangeNode.For,
-				Key:    rangeNode.Key,
-				Value:  rangeNode.Value,
-				TokPos: rangeNode.TokPos,
-				Tok:    rangeNode.Tok,
-				Body:   newBody,
-				X:      rangeNode.X,
-			}
-
-			mutations = append(mutations, mutator.Mutation{
-				Change: func() {
-					list[listIndex] = newNode
-				},
-				Reset: func() {
-					list[listIndex] = oldNode
-				},
-			})
-		}
+	for _, nn := range n.Body.List {
+		newBody.List = append(newBody.List, nn)
 	}
 
-	return mutations
+	return []mutator.Mutation{
+		{
+			Change: func() {
+				n.Body = newBody
+			},
+			Reset: func() {
+				n.Body = oldBody
+			},
+		},
+	}
 }
