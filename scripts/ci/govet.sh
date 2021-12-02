2ee280104a1217834550a2d21a26a394cbfb63ce
Vasiliy Yudin <vvyudin@avito.ru>
2021-12-02 17:05:41 +0300

    CTHL-2084 rollback

--- scripts/ci/govet.sh
+++ scripts/ci/govet.sh
@@ -4,7 +4,7 @@ if [ -z ${PKG+x} ]; then echo "PKG is not set"; exit 1; fi
 if [ -z ${ROOT_DIR+x} ]; then echo "ROOT_DIR is not set"; exit 1; fi
 
 echo "go vet:"
-OUT=$(go vet -all=true ./... 2>&1 | grep --invert-match -E "(Checking file|\%p of wrong type|use of internal package|can't check non-constant format|/example)")
+OUT=$(go vet -all=true ./... 2>&1 | grep --invert-match -E "(Checking file|\%p of wrong type|can't check non-constant format|/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 if [ -n "$PROBLEM" ]; then exit 1; fi
