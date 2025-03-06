a7c9025adb9724c41da46c2c22bd499b8b223cae
Aleksandr Asmakov <avasmakov@avito.ru>
2025-03-06 15:42:00 +0300

    CTHL-3917: upd go version to 1.23

--- scripts/ci/errcheck.sh
+++ scripts/ci/errcheck.sh
@@ -3,10 +3,10 @@
 if [ -z ${PKG+x} ]; then echo "PKG is not set"; exit 1; fi
 if [ -z ${ROOT_DIR+x} ]; then echo "ROOT_DIR is not set"; exit 1; fi
 
-echo ${PATH_TO_GO_BINARY}
-echo "\n"
 echo "errcheck:"
-OUT=$(${PATH_TO_GO_BINARY}/errcheck $PKG/... 2>&1 | grep --invert-match -E "(/example)")
+echo ${PATH}
+echo ${GOPATH}
+OUT=$(${GOPATH}/errcheck $PKG/... 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 if [ -n "$PROBLEM" ]; then exit 1; fi
