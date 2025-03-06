ce18fac2e5c0852858966023bf4384477c840727
Aleksandr Asmakov <avasmakov@avito.ru>
2025-03-06 16:41:18 +0300

    CTHL-3917: upd go version to 1.23

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -3,8 +3,8 @@
 if [ -z ${PKG+x} ]; then echo "PKG is not set"; exit 1; fi
 if [ -z ${ROOT_DIR+x} ]; then echo "ROOT_DIR is not set"; exit 1; fi
 
-echo "golangci-lint:"
-OUT=$(golangci-lint run $PKG/... 2>&1 | grep --invert-match -E "(/example)")
+echo "golint:"
+OUT=$(golint $PKG/... 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 if [ -n "$PROBLEM" ]; then exit 1; fi
